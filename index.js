498f831605623c5b779031db40dd93fb2c2a77c1
Samwel <samwelmwangi106@gmail.com>
2026-03-25 00:31:43 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 
+//calculate tax
+function calculateTax(amount){
+    return amount*0.10;
+}
 
+//convert to uppercase
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+
+//Find maximum
+function findMaximum(num1,num2){
+    if (num1 > num2){
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
+
+//is palindrome
+function isPalindrome(word){
+    let reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
+//calculate discounted price
+function calculateDiscountedPrice(originalPrice,discountPercentage){
+    let discount = (discountPercentage/100)*originalPrice;
+    return originalPrice - discount;
+}
 
 
 
